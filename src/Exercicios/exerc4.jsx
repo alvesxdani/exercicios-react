da7580b3d44706d825bb0bd59e4039ec5cba38fe
alvesxdani <dani.alvesm22@gmail.com>
2023-04-07 11:40:50 -0300

    Finish the exercise about useEffect

--- src/Exercicios/exerc4.jsx
+++ src/Exercicios/exerc4.jsx
@@ -5,24 +5,39 @@
 // Defina o produto clicado como uma preferência do usuário no localStorage
 // Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Exerc4 = () => {
-  const [dados, setDados] = useState();
-
+  const [produto, setProduto] = useState(window.localStorage.getItem('produto'));
+  const [dados, setDados] = useState(null);
   
+  useEffect(() => {
+    if(produto !== null) {
+      window.localStorage.setItem('produto', produto);
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+      .then(response => response.json())
+      .then(json => setDados(json))
+    }
+  },[produto])
+
+  async function handleClick({target}) {
+    setProduto(target.innerText);
+  }
 
   return (
     <>
     <div className="box">
-      <h1>Selecione sua preferência:</h1>
-      <button>Notebook</button>
-      <button>Smartphone</button>
+      <h1>Selecione sua preferência: {produto}</h1>
+      <button onClick={handleClick}>Notebook</button>
+      <button onClick={handleClick}>Smartphone</button>
     </div>
 
-    <div className="box">
-
+    {dados !== null ? (
+      <div className="box">
+      <h1>{dados.nome}</h1>
+      <p>{dados.preco}</p>
     </div>
+    ) : null}
     </>
   )
 }
